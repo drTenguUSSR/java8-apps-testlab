8a23a41f85ed3808355a809fb804e90b2c835ff3
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2024-09-29 00:01:29 +0300

    init default Spring Initializr. maven, java 17,SpringBoot 3.2.10
    deps: Spring Web, Spring Data JPA, Liquibase Migration, Lombok, Thymeleaf, Spring Boot DevTools
    
    origin: supply-warehouse-j8-sb2.zip

---
+++ src/main/java/mil/teng/q2024/supply/warehouse/SupplyWarehouseJ8Sb2Application.java
@@ -0,0 +1,13 @@
+package mil.teng.q2024.supply.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyWarehouseJ8Sb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyWarehouseJ8Sb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/mil/teng/q2024/supply/warehouse/SupplyWarehouseJ8Sb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mil.teng.q2024.supply.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyWarehouseJ8Sb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
