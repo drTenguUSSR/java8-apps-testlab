838e3906ca6b51d5fe13960de38fb423bdfb7cdb
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2024-11-30 23:51:02 +0300

    init subproject sub-prj-animals-by-classes

---
+++ sub-prj-animals-by-classes/src/main/java/mil/teng/q2024/animals4classes/App.java
@@ -0,0 +1,10 @@
+package mil.teng.q2024.animals4classes;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
