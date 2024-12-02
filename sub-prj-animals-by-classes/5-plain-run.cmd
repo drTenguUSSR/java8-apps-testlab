f94c70de8351cf6074930f1fa77eec235174c924
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2024-12-02 11:29:32 +0300

    sp.abc. correct loging to console and file

--- sub-prj-animals-by-classes/5-plain-run.cmd
+++ sub-prj-animals-by-classes/5-plain-run.cmd
@@ -1,4 +1,4 @@
 setlocal
 set remote_debug=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:46753
-java %remote_debug% -Djava.io.tmpdir=tmpFolder -jar target/sub-prj-animals-by-classes-1.0-SNAPSHOT.jar
+java %remote_debug% -Djava.io.tmpdir=tmpFolder -Dlog4j2.configurationFile=log4j2.xml -jar target/sub-uber.jar
 endlocal
--- sub-prj-animals-by-classes/src/main/java/mil/teng/q2024/animals4classes/App.java
+++ sub-prj-animals-by-classes/src/main/java/mil/teng/q2024/animals4classes/App.java
@@ -1,10 +1,13 @@
 package mil.teng.q2024.animals4classes;
 
-/**
- * Hello world!
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        logger.info("Hello World-22");
     }
 }
