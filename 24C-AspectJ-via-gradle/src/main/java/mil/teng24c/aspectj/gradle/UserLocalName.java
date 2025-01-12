0061dca0cc0cc72008076f4461f0be5c84175736
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2025-01-12 21:41:48 +0300

    24c. disable lombok

--- 24C-AspectJ-via-gradle/src/main/java/mil/teng24c/aspectj/gradle/UserLocalName.java
+++ 24C-AspectJ-via-gradle/src/main/java/mil/teng24c/aspectj/gradle/UserLocalName.java
@@ -1,14 +1,56 @@
 package mil.teng24c.aspectj.gradle;
 
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
 public class UserLocalName {
-    String first;
-    String last;
-    int age;
+    private String first;
+    private String last;
+    private int age;
+
+    public UserLocalName() {
+    }
+
+    public UserLocalName(String first, String last, int age) {
+        this.first = first;
+        this.last = last;
+        this.age = age;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLocalName{" +
+                "first='" + first + '\'' +
+                ", last='" + last + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
