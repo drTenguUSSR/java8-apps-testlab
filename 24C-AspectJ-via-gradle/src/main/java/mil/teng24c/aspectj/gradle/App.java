adde15a327c848a2ecb845a961e20c1e058e76e3
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2025-02-01 16:13:19 +0300

    24c. update run via IDE description

--- 24C-AspectJ-via-gradle/src/main/java/mil/teng24c/aspectj/gradle/App.java
+++ 24C-AspectJ-via-gradle/src/main/java/mil/teng24c/aspectj/gradle/App.java
@@ -6,6 +6,12 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * run via ide: run configuration/VM options:
+ *  -Dvar123=ide-vm-opt
+ *  -Djava.io.tmpdir=tmpFolder
+ *  -Dlog4j2.configurationFile=config/log4j2.xml
+ */
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) throws IOException {
