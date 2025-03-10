ab5ab819b14a5af677bb3475943c8273d535c82e
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2025-03-10 23:08:47 +0300

    253.SpringBootHello project init (not build)

---
+++ 253_SpringBootHello/src/main/java/mil/teng253/sringboothello/SringboothelloApplication.java
@@ -0,0 +1,13 @@
+package mil.teng253.sringboothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringboothelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringboothelloApplication.class, args);
+	}
+
+}
---
+++ 253_SpringBootHello/src/test/java/mil/teng253/sringboothello/SringboothelloApplicationTests.java
@@ -0,0 +1,13 @@
+package mil.teng253.sringboothello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringboothelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
